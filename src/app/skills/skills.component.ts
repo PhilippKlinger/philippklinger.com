2e033c98497493f847540faa7464f7d7a62b46c4
PhilippKlinger <klinger.breuberg@web.de>
2023-10-17 12:17:50 +0100

    added abovethefold description and skills section

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent {
+  icons = ['./assets/icons/angular.svg',
+    './assets/icons/ts.svg',
+    './assets/icons/js.svg',
+    './assets/icons/html.svg',
+    './assets/icons/css.svg',
+    './assets/icons/firebase.svg',
+    './assets/icons/git.svg',
+    './assets/icons/scrum.svg',
+    './assets/icons/api.svg',
+    './assets/icons/material.svg',];
+  skills = ['Agular', 'Typescript',
+    'JavaScript', 'HTML', 'CSS',
+    'Firebase', 'Git', 'Scrum',
+    'Rest-Api', 'Material'];
 
 }
