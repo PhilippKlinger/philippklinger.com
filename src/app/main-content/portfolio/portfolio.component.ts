10553f64c21933e682f8aaa99077b3e999793378
PhilippKlinger <klinger.breuberg@web.de>
2023-11-17 15:30:57 +0000

    improved overall features added animations added first projects

--- src/app/main-content/portfolio/portfolio.component.ts
+++ src/app/main-content/portfolio/portfolio.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./portfolio.component.scss']
 })
 export class PortfolioComponent {
-
+  projects = [
+    {
+      id: 1,
+      title: 'Join',
+      technologies: 'Angular | TypeScript | HTML | CSS',
+      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories',
+      githubLink: 'https://github.com/yourusername/join',
+      liveLink: 'https://yourwebsite.com/join',
+      imageUrl: './assets/projects/mayavaliente.png'
+    },
+    {
+      id: 2,
+      title: 'Browsergame',
+      technologies: 'Angular | TypeScript | HTML | CSS',
+      description: 'Jump and Run Browsergame',
+      githubLink: 'https://github.com/yourusername/join',
+      liveLink: 'https://yourwebsite.com/join',
+      imageUrl: './assets/projects/mayavaliente.png'
+    },
+    {
+      id: 3,
+      title: 'DA Bubble',
+      technologies: 'Angular | TypeScript | HTML | CSS',
+      description: 'Jump and Run Browsergame',
+      githubLink: 'https://github.com/yourusername/join',
+      liveLink: 'https://yourwebsite.com/join',
+      imageUrl: './assets/projects/mayavaliente.png'
+    },
+    {
+      id: 4,
+      title: 'Simple Crm',
+      technologies: 'Angular | TypeScript | HTML | CSS',
+      description: 'Jump and Run Browsergame',
+      githubLink: 'https://github.com/yourusername/join',
+      liveLink: 'https://yourwebsite.com/join',
+      imageUrl: './assets/projects/mayavaliente.png'
+    },
+  ];
 }
