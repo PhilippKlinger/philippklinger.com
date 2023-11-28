1a8f941f4ce3542a5aa4cfb4100573a15021b205
PhilippKlinger <klinger.breuberg@web.de>
2023-11-28 14:42:43 +0000

    added projects content / added links / footer socials working

--- src/app/main-content/portfolio/portfolio.component.ts
+++ src/app/main-content/portfolio/portfolio.component.ts
@@ -10,38 +10,38 @@ export class PortfolioComponent {
     {
       id: 1,
       title: 'Join',
-      technologies: 'Angular | TypeScript | HTML | CSS',
+      technologies: 'JavaScript | HTML | CSS',
       description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories',
-      githubLink: 'https://github.com/yourusername/join',
-      liveLink: 'https://yourwebsite.com/join',
-      imageUrl: './assets/projects/mayavaliente.png'
+      githubLink: 'https://github.com/PhilippKlinger/Join',
+      liveLink: 'https://projectsphilipp.leahannich.de/Join/templates/html/login.html',
+      imageUrl: './assets/projects/join.png'
     },
     {
       id: 2,
       title: 'Browsergame',
-      technologies: 'Angular | TypeScript | HTML | CSS',
+      technologies: 'JavaScript | HTML | CSS',
       description: 'Jump and Run Browsergame',
-      githubLink: 'https://github.com/yourusername/join',
-      liveLink: 'https://yourwebsite.com/join',
+      githubLink: 'https://github.com/PhilippKlinger/Browsergame',
+      liveLink: 'https://projectsphilipp.leahannich.de/browsergame/index.html',
       imageUrl: './assets/projects/mayavaliente.png'
     },
     {
       id: 3,
-      title: 'DA Bubble',
-      technologies: 'Angular | TypeScript | HTML | CSS',
-      description: 'Jump and Run Browsergame',
-      githubLink: 'https://github.com/yourusername/join',
-      liveLink: 'https://yourwebsite.com/join',
-      imageUrl: './assets/projects/mayavaliente.png'
+      title: 'Pokedex',
+      technologies: 'JavaScript | HTML | CSS | API',
+      description: 'Pokedex',
+      githubLink: 'https://github.com/PhilippKlinger/Pokedex',
+      liveLink: 'https://projectsphilipp.leahannich.de/Pokedex/index.html',
+      imageUrl: './assets/projects/pokedex.png'
     },
     {
       id: 4,
-      title: 'Simple Crm',
+      title: 'DA Bubble',
       technologies: 'Angular | TypeScript | HTML | CSS',
-      description: 'Jump and Run Browsergame',
+      description: 'Work in Progress',
       githubLink: 'https://github.com/yourusername/join',
       liveLink: 'https://yourwebsite.com/join',
-      imageUrl: './assets/projects/mayavaliente.png'
+      imageUrl: './assets/projects/da-bubble.jpg'
     },
   ];
 
